866dadc7904ef376067153adf91237256e69cfc1
daniedu <daniedu@gmail.com>
2026-08-30 10:07:05 -0500

    adding new colours and aseprite

--- modules/misc.nix
+++ modules/misc.nix
@@ -1,15 +1,23 @@
 { pkgs, ... }: {
   nix.settings = {
-    experimental-features = [ "nix-command" "flakes" ];
+    experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
     extra-substituters = [ "https://nvf.cachix.org" ];
     extra-trusted-public-keys = [ "nvf.cachix.org-1:GMQWiUhZ6ux9D5CvFFMwnc2nFrUHTeGaXRlVBXo+naI=" ];
   };
 
   nixpkgs.config.allowUnfree = true;
 
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+
   stylix = {
     enable = true;
     autoEnable = false;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/material.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/arroz-con-dulce-dark.yaml";
   };
 }
--- modules/packages.nix
+++ modules/packages.nix
@@ -13,5 +13,6 @@
     unrar
 
     opencode
+    aseprite
   ];
 }
